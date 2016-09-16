ea0adcd1999cd365f4aaf21a20f9995f171ee871
Markus Ankenbrand <markus@ankenbrand.me>
2016-09-16 17:43:37 +0200

    Add function isKeyPressed

--- src/org/iimog/mfg/Mfg.ts
+++ src/org/iimog/mfg/Mfg.ts
@@ -3,6 +3,8 @@ class Mfg
     public static main():void
     {
         MfgDebug.log("Mfg");
+        MfgKey.init();
+        // window.setInterval(() => {if(MfgKey.isKeyPressed(MfgKey.KEY_DOWN)){MfgDebug.log('Down gedruckt.')}else{MfgDebug.log('Down nicht gedruckt.')}}, 500)
     }
 }
 
--- src/org/iimog/mfg/MfgKey.ts
+++ src/org/iimog/mfg/MfgKey.ts
@@ -1,19 +1,40 @@
 class MfgKey
 {
+    public static KEY_UP:number = 38;
+    public static KEY_DOWN:number = 40;
+    public static KEY_LEFT:number = 37;
+    public static KEY_RIGHT:number = 39;
+
+    private static currentlyPressed:Array<number> = [];
+
     public static keyPressed(keyCode:number):void
     {
-        MfgDebug.log('Key pressed: '+keyCode);
+        if(!MfgKey.isKeyPressed(keyCode)){
+            MfgKey.currentlyPressed.push(keyCode)
+        }
+        // MfgDebug.log('Key pressed: '+keyCode);
     }
+
     public static keyReleased(keyCode:number):void
     {
-        MfgDebug.log('Key released: '+keyCode);
+        if(MfgKey.isKeyPressed(keyCode)){
+            MfgKey.currentlyPressed.splice(MfgKey.currentlyPressed.indexOf(keyCode), 1);
+        }
+        // MfgDebug.log('Key released: '+keyCode);
+    }
+
+    public static init():void
+    {
+        window.addEventListener("keydown", (evt) => {
+            MfgKey.keyPressed(evt.which);
+        }, false);
+        window.addEventListener("keyup", (evt) => {
+            MfgKey.keyReleased(evt.which);
+        }, false);
+    }
+
+    public static isKeyPressed(keyCode:number):boolean{
+        return MfgKey.currentlyPressed.indexOf(keyCode) >= 0;
     }
 }
 
-window.addEventListener("keydown", (evt) => {
-    MfgKey.keyPressed(evt.which);
-}, false);
-
-window.addEventListener("keyup", (evt) => {
-    MfgKey.keyReleased(evt.which);
-}, false);
